09d4d406002356d84ce7a5d0e5842a2a2f89d8ef
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2026-03-04 13:07:41 -0600

    feat: initial aplication settings

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-from flask import Flask, render_template
+from src.app import app
 
-app=Flask(__name__)
-
-@app.route("/")
-def index():
-    return render_template("index.html")
-
-if __name__ =="__main__":
-    app.run(debug=True,port=3030)
+if __name__ == "__main__":
+    app.run(debug=True, port=3030)
