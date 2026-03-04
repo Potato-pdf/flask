fee8b9ae72f4498afaa66e6e719d8ed43a4db4e2
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2026-03-04 14:24:44 -0600

    feat: db connection

--- src/app.py
+++ src/app.py
@@ -1,6 +1,11 @@
 from flask import Flask
 from .routes.contacts import contacts_bp
+from flask_sqlalchemy import SQLAlchemy
+
+
 
 app=Flask(__name__)
-
+app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 app.register_blueprint(contacts_bp)
