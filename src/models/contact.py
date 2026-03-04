16f8a068f0034e4a106635e2c66a1b28956da23e
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2026-03-04 14:50:00 -0600

    feat: init en models

--- src/models/contact.py
+++ src/models/contact.py
@@ -7,3 +7,16 @@ class Contact(db.Model):
     name = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
     phone = db.Column(db.String(20), unique=True, nullable=False)
+    
+    
+    """
+    Funcion init redundante, util para agregar validaciones
+    Ejemplo: 
+            self.name = name.strip().title()  # Limpia y capitaliza
+            self.email = email.strip().lower()  # Limpia y convierte a minúsculas
+            self.phone = phone.strip()  # Limpia espacios en blanco
+    """
+    def __init__(self, name, email, phone):
+        self.name = name
+        self.email = email
+        self.phone = phone
